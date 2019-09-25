d6c93fdc286f5855983f04fc4f7c3eebbdb443da
Shaariq Chundrigar <shaariq.chundrigar@nextgen.net>
2019-09-25 14:00:48 +1000

    Added express server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const bodyParser = require("body-parser");
-const pino = require("express-pino-logger")();
 var cors = require("cors");
 var request = require("request");
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get("/authorization", (req, res) => {
   var authOptions = {
@@ -30,6 +32,14 @@ app.get("/authorization", (req, res) => {
   });
 });
 
-app.listen(3001, () =>
-  console.log("Express server is running on localhost:3001")
-);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
